93bbebd4bde360584e758372b8266a55e1170a6c
dFriggi <114791461+dFriggi@users.noreply.github.com>
2024-02-19 18:11:47 -0300

    Delete qterceiro teste.js

--- qterceiro teste.js
+++
@@ -1,8 +0,0 @@
-createDatabase("Davi" , "davifriggi@gmail.com" , "amor3009")
-
-function createDatabase(databaseName , email , pass){
-console.log(`Congratulations ${databaseName}!
-Your account has been registered with the folllowing data.
-Email: ${email};
-Pass: ${pass};`)
-}
